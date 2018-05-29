a7bcc8674d8b176c4d211885d5534d1af1fbe790
alicelab <stephane@alicelab.fr>
2018-05-29 16:45:28 +0200

    robot revient

--- robot.gd
+++ robot.gd
@@ -1,9 +1,26 @@
 extends Area2D
 
+# TODO
+# faire rebondir le robot
+var flip = false
+
 func _physics_process(delta):
-	position.x = position.x +1
+	
 	$Sprite.play("Run")
 	
+	if (position.x < 262 && flip == false):
+		position.x = position.x +1
+		#flip = true
+	else:
+		flip = true
+		$Sprite.flip_h = true
+		position.x = position.x -1
+	
+	if (position.x < 16  && flip == true):
+		flip = false
+		$Sprite.flip_h = false
+		position.x = position.x +1	
+		
 	var bodies = get_overlapping_bodies()
 	
 	for body in bodies:
@@ -14,3 +31,4 @@ func _physics_process(delta):
 		position.y = 172
 	else:
 		position.y = 182
+		
